4c0d0bfab87aa08ca72ee137f39ff9458e65d4b1
Yuan Luo <yuan.luo@hotmail.com>
2020-10-21 14:40:52 +0800

    Create 剑指 Offer 37. 序列化二叉树.cpp

---
+++ 二叉树/剑指 Offer 37. 序列化二叉树.cpp
@@ -0,0 +1,71 @@
+请实现两个函数，分别用来序列化和反序列化二叉树。
+
+示例: 
+
+你可以将以下二叉树：
+
+    1
+   / \
+  2   3
+     / \
+    4   5
+
+序列化为 "[1,2,3,null,null,4,5]"
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/xu-lie-hua-er-cha-shu-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+=====================================================================
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream in(data);
+        return deserialize(in);
+    }
+
+private:
+    void serialize(TreeNode* root, ostringstream &out) {
+        if (root == nullptr) {
+            out << "# ";
+            return;
+        }
+        out << root->val <<  " ";
+        serialize(root->left, out);
+        serialize(root->right, out);
+    }
+
+    TreeNode* deserialize(istringstream &in) {
+        string val;
+        in >> val;
+        if (val == "#") return nullptr;
+        TreeNode *root = new TreeNode(stoi(val));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
+
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
