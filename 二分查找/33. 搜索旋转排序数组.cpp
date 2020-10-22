bcdaeb3d041b3004e5a846963b41e87e17c9bfaa
Yuan Luo <yuan.luo@hotmail.com>
2020-10-22 19:50:54 +0800

    Create 33. 搜索旋转排序数组.cpp

---
+++ 二分查找/33. 搜索旋转排序数组.cpp
@@ -0,0 +1,54 @@
+33. 搜索旋转排序数组
+
+https://leetcode-cn.com/problems/search-in-rotated-sorted-array/
+
+给你一个升序排列的整数数组 nums ，和一个整数 target 。
+
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。（例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] ）。
+
+请你在数组中搜索 target ，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+
+ 
+示例 1：
+
+输入：nums = [4,5,6,7,0,1,2], target = 0
+输出：4
+示例 2：
+
+输入：nums = [4,5,6,7,0,1,2], target = 3
+输出：-1
+示例 3：
+
+输入：nums = [1], target = 0
+输出：-1
+ 
+
+提示：
+1 <= nums.length <= 5000
+-10^4 <= nums[i] <= 10^4
+nums 中的每个值都 独一无二
+nums 肯定会在某个点上旋转
+-10^4 <= target <= 10^4
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < nums[right]) {
+                if (nums[mid] < target && target <= nums[right]) left = mid + 1;
+                else right = mid - 1;
+            } else if (nums[mid] >= nums[right]) {
+                if (target >= nums[left] && nums[mid] > target) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+};
+
+
