ec10ac686198e6d6baa693f83c956a4d9e60f355
Yuan Luo <yuan.luo@hotmail.com>
2020-10-22 19:47:27 +0800

    Create 34. 在排序数组中查找元素的第一个和最后一个位置.cpp

---
+++ 二分查找/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,56 @@
+34. 在排序数组中查找元素的第一个和最后一个位置
+
+给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+
+你的算法时间复杂度必须是 O(log n) 级别。
+
+如果数组中不存在目标值，返回 [-1, -1]。
+
+示例 1:
+
+输入: nums = [5,7,7,8,8,10], target = 8
+输出: [3,4]
+示例 2:
+
+输入: nums = [5,7,7,8,8,10], target = 6
+输出: [-1,-1]
+
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res = {-1, -1};
+        int idxLeft = searchTargetIndex(nums, target, true);
+
+        if (idxLeft == nums.size() || nums[idxLeft] != target) return res;
+
+        int idxRight = searchTargetIndex(nums, target, false);
+        res[0] = idxLeft;
+        res[1] = idxRight - 1;
+        return res;
+    }
+
+    int searchTargetIndex(vector<int>& nums, int target, bool left) {
+        int lo = 0;
+        int hi = nums.size();
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > target || (left && nums[mid] == target)) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return lo;
+    }
+};
+
+
+
+
+
+
+
+
+
+
