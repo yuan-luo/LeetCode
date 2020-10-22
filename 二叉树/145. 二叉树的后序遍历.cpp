bcae08e683630f2eb8e2d64bfabd57fe27ebfd46
Yuan Luo <yuan.luo@hotmail.com>
2020-10-22 22:14:27 +0800

    Create 145. 二叉树的后序遍历.cpp

---
+++ 二叉树/145. 二叉树的后序遍历.cpp
@@ -0,0 +1,72 @@
+145. 二叉树的后序遍历
+
+https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+
+给定一个二叉树，返回它的 后序 遍历。
+
+题解：
+两种解法：迭代和倒序
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        TreeNode *head = root;
+        while(!st.empty()) {
+            TreeNode *p = st.top();
+            if ( (p->left == NULL && p->right == NULL) || p->left == head || p->right == head) {
+                st.pop();
+                res.push_back(p->val);                
+                head = p;
+            } else {
+                if (p->right) st.push(p->right);
+                if (p->left) st.push(p->left);   
+            }           
+        }
+        return res;
+    }
+};
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if (!root) return res;
+        stack<TreeNode*> nodeStack;
+        nodeStack.push(root);
+        
+        while (!nodeStack.empty()) {
+            TreeNode *node = nodeStack.top();
+            res.push_back(node->val);
+            nodeStack.pop();
+            if (node->left) 
+                nodeStack.push(node->left);
+            if (node->right)
+                nodeStack.push(node->right);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
