c3b2e99d73b767e5d82aad261c2c8781738f8c6c
Yuan Luo <yuan.luo@hotmail.com>
2020-10-22 21:15:14 +0800

    Create 173. 二叉搜索树迭代器.cpp

---
+++ 设计/173. 二叉搜索树迭代器.cpp
@@ -0,0 +1,27 @@
+173. 二叉搜索树迭代器
+
+https://leetcode-cn.com/problems/binary-search-tree-iterator/
+
+实现一个二叉搜索树迭代器。你将使用二叉搜索树的根节点初始化迭代器。
+
+调用 next() 将返回二叉搜索树中的下一个最小的数。
+
+示例：
+
+           7
+         /    \
+        3     15
+            /    \
+           9      20
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // 返回 3
+iterator.next();    // 返回 7
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 9
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 15
+iterator.hasNext(); // 返回 true
+iterator.next();    // 返回 20
+iterator.hasNext(); // 返回 false
+
