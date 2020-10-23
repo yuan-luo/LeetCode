87bbcc2ed4866b1fb438bc9374c99ab5cff91b24
Yuan Luo <yuan.luo@hotmail.com>
2020-10-23 21:43:05 +0800

    Create *315. 计算右侧小于当前元素的个数.cpp

---
+++ 二分查找/*315. 计算右侧小于当前元素的个数.cpp
@@ -0,0 +1,17 @@
+315. 计算右侧小于当前元素的个数
+
+https://leetcode-cn.com/problems/count-of-smaller-numbers-after-self/
+
+给定一个整数数组 nums，按要求返回一个新数组 counts。数组 counts 有该性质： counts[i] 的值是  nums[i] 右侧小于 nums[i] 的元素的数量。
+
+示例：
+
+输入：nums = [5,2,6,1]
+输出：[2,1,1,0] 
+解释：
+5 的右侧有 2 个更小的元素 (2 和 1)
+2 的右侧仅有 1 个更小的元素 (1)
+6 的右侧有 1 个更小的元素 (1)
+1 的右侧有 0 个更小的元素
+ 
+
