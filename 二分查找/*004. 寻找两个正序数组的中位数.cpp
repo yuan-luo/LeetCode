78d64059ebbb0b6873295604f3db2c8d83cc86f5
Yuan Luo <yuan.luo@hotmail.com>
2020-10-24 00:18:44 +0800

    Update *004. 寻找两个正序数组的中位数.cpp

--- 二分查找/*004. 寻找两个正序数组的中位数.cpp
+++ 二分查找/*004. 寻找两个正序数组的中位数.cpp
@@ -15,38 +15,36 @@ public:
         int n = nums2.size();
 
         // 分割线左边的所有元素需要满足的个数(m + n + 1)/2
-        int k = (m + n + 1) / 2;
+        int totalLeft = (m + n + 1) / 2;
 
         // 在num1的区间[0, m]里面查找适当的分割线
-        // 使得nums1[i - 1] <= nums2[j] 分割线在第一个数数组左边元素的数值要小于分割线在第二个数组右边的元素的数值，注意-1的表达表示在分割线的左边
-        // && nums2[j - 1] <= nums1[i] 分割线在第二个数数组左边元素的数值要小于分割线在第一个数组右边的元素的数值，
         int left = 0;
         int right = m;
+
         while (left < right) {
-            int m1 = left + (right - left) / 2;
-            int m2 = k - m1;
-            if (nums1[m1] < nums2[m2 - 1]) {
-                // 下一轮的搜索区间[left, i - 1]
-                left = m1 + 1;
+            int i = left + (right - left) / 2;
+            int j = totalLeft - i;
+            if (nums1[i] < nums2[j - 1]) {
+                left = i + 1;
             } else {
-                // 下一轮的搜索区间[i, right]
-                right = m1;
+                right = i;
             }
         }
 
-        int m1 = left;
-        int m2 = k - left;
-        int c1 = max(m1 <= 0 ? INT_MIN:nums1[m1 - 1],
-                    m2 <= 0 ? INT_MIN:nums2[m2 - 1]);
-
+        int i = left;
+        int j = totalLeft - i;
+        int nums1LeftMax = (i <= 0) ? INT_MIN : nums1[i - 1];
+        int nums1RightMin = (i >= m) ? INT_MAX : nums1[i];
+        int nums2LeftMax = (j <= 0) ? INT_MIN : nums2[j - 1];
+        int nums2RightMin = (j >= n) ? INT_MAX : nums2[j];
+     
         if ((m + n) % 2 == 1)
-            return c1;
-        int c2 = min(m1 >= m ? INT_MAX: nums1[m1],
-                      m2 >= n ? INT_MAX : nums2[m2]);
-        return (c1 + c2) * 0.5;
-    }
-};
+            return max(nums1LeftMax, nums2LeftMax);
 
+        else
+            return (double) (max(nums1LeftMax, nums2LeftMax) + min(nums1RightMin, nums2RightMin)) * 0.5;
+    }
+}; 
 
 另外一种类似解法：
 ============================================================
