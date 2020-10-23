d0f11870827e52e2ce729a72bc27586d1e492c26
Yuan Luo <yuan.luo@hotmail.com>
2020-10-23 21:27:38 +0800

    Create 剑指 Offer 20. 表示数值的字符串.cpp

---
+++ 数学/剑指 Offer 20. 表示数值的字符串.cpp
@@ -0,0 +1,14 @@
+剑指 Offer 20. 表示数值的字符串
+
+https://leetcode-cn.com/problems/biao-shi-shu-zhi-de-zi-fu-chuan-lcof/
+
+请实现一个函数用来判断字符串是否表示数值（包括整数和小数）。
+例如，字符串"+100"、"5e2"、"-123"、"3.1416"、"-1E-16"、"0123"都表示数值，但"12e"、"1a3.14"、"1.2.3"、"+-5"及"12e+5.4"都不是。
+
+class Solution {
+public:
+    bool isNumber(string s) {
+        
+    }
+};
+
