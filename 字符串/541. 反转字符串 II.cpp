cc077592ef3333eebd49c95662948962a9048585
Yuan Luo <yuan.luo@hotmail.com>
2020-10-23 16:54:54 +0800

    Create 541. 反转字符串 II.cpp

---
+++ 字符串/541. 反转字符串 II.cpp
@@ -0,0 +1,29 @@
+541. 反转字符串 II
+
+给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。
+
+如果剩余字符少于 k 个，则将剩余字符全部反转。
+如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。
+ 
+
+示例:
+
+输入: s = "abcdefg", k = 2
+输出: "bacdfeg"
+
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        for (int i = 0; i < s.size(); i += (2 * k)) {
+            // 1. 每隔 2k 个字符的前 k 个字符进行反转
+            // 2. 剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符
+            if (i + k <= s.size()) {
+                reverse(s.begin() + i, s.begin() + i + k);
+                continue;
+            }
+            // 3. 剩余字符少于 k 个，则将剩余字符全部反转。
+            reverse(s.begin() + i, s.begin() + s.size());
+        }
+        return s;
+    }
+};
