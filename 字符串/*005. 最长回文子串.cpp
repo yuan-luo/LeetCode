4a763ab0e65e59cf245740eced734abe5b4e7ed2
Yuan Luo <yuan.luo@hotmail.com>
2020-10-23 22:40:01 +0800

    Create *005. 最长回文子串.cpp

---
+++ 字符串/*005. 最长回文子串.cpp
@@ -0,0 +1,22 @@
+5. 最长回文子串
+
+https://leetcode-cn.com/problems/longest-palindromic-substring/
+
+给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+示例 1：
+
+输入: "babad"
+输出: "bab"
+注意: "aba" 也是一个有效答案。
+示例 2：
+
+输入: "cbbd"
+输出: "bb"
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+    }
+};
