7f4842cf430f454d1bb9554ad64201cd562d120f
Yuan Luo <yuan.luo@hotmail.com>
2020-10-23 18:01:44 +0800

    Create 剑指 Offer 48. 最长不含重复字符的子字符串.cpp

---
+++ 滑动窗口/剑指 Offer 48. 最长不含重复字符的子字符串.cpp
@@ -0,0 +1,37 @@
+剑指 Offer 48. 最长不含重复字符的子字符串
+
+请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+class Solution {
+public:
+
+    int lengthOfLongestSubstring(string s) {
+        int res = 0, left = -1, n = s.size();
+        unordered_map<int, int> m; // 字符:下标
+        for (int i = 0; i < n; ++i) {
+            if (m.count(s[i]) && m[s[i]] > left) {
+                left = m[s[i]];  // m[s[i]]表示字符为s[i]的下标
+            }
+            m[s[i]] = i;
+            res = max(res, i - left);
+        }
+        return res;
+    }
+};
