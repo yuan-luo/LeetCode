1bb8650cb4b16323afbb007670cfbf0057f70c1c
Yuan Luo <yuan.luo@hotmail.com>
2020-10-24 21:32:08 +0800

    Create 75. 颜色分类.cpp

---
+++ 双指针/75. 颜色分类.cpp
@@ -0,0 +1,35 @@
+75. 颜色分类
+
+https://leetcode-cn.com/problems/sort-colors/
+
+给定一个包含红色、白色和蓝色，一共 n 个元素的数组，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
+
+此题中，我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
+
+注意:
+不能使用代码库中的排序函数来解决这道题。
+
+示例:
+
+输入: [2,0,2,1,1,0]
+输出: [0,0,1,1,2,2]
+
+=====================================================
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int p0 = 0, p2 = nums.size() - 1;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (i <= p2 && nums[i] == 2) {
+                swap(nums[i], nums[p2]);
+                --p2;
+            }
+            if (nums[i] == 0) {
+                swap(nums[i], nums[p0]);
+                ++p0;
+            }
+        }
+    }
+};
