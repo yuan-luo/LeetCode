e45f0ed549554ef2df7001bc8238d92db0f72887
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 00:09:53 +0800

    Update *80. 删除排序数组中的重复项 II.cpp

--- 双指针/*80. 删除排序数组中的重复项 II.cpp
+++ 双指针/*80. 删除排序数组中的重复项 II.cpp
@@ -16,5 +16,40 @@ https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array-ii/
 输出：7, nums = [0,0,1,1,2,3,3]
 解释：函数应返回新长度 length = 7, 并且原数组的前五个元素被修改为 0, 0, 1, 1, 2, 3, 3 。 你不需要考虑数组中超出新长度后面的元素。
  
+=============================================================
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 2) return n;
+
+        int slow = 1;
+        for (int fast = 2; fast < n; ++fast) {
+            if (nums[fast] != nums[slow - 1]) { // <<<<<<<<<< 和下面这道题相比，区别在这里：比较的下标是slow - 1，而那道题是和slow做比较。
+                ++slow;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+};
+
+对比 删除排序数组中的重复项 这道题的解法，体会一下区别。
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.size(); ++j) {
+            if (nums[i] != nums[j]) {
+                ++i;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+};
 
 
