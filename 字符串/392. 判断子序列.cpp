60574dc8efe327d78ac39009943b91646f99bb8b
Yuan Luo <yuan.luo@hotmail.com>
2020-10-24 17:50:35 +0800

    Create 392. 判断子序列.cpp

---
+++ 字符串/392. 判断子序列.cpp
@@ -0,0 +1,34 @@
+392. 判断子序列
+
+给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+你可以认为 s 和 t 中仅包含英文小写字母。字符串 t 可能会很长（长度 ~= 500,000），而 s 是个短字符串（长度 <=100）。
+
+示例 1:
+s = "abc", t = "ahbgdc"
+
+返回 true.
+
+示例 2:
+s = "axc", t = "ahbgdc"
+
+返回 false.
+
+========================================
+双指针i和j分别指向两个string。
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+        while (i < s.size() && j < t.size()) {
+            if (s[i] == t[j]) {
+                ++i;
+                ++j;
+            } else {
+                ++j;
+            }
+        }
+        return i == s.size();
+    }
+};
