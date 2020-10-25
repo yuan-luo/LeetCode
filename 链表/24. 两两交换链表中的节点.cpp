7aea594218c94964e5a840346302f5db53dcb18c
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 22:08:35 +0800

    Create 24. 两两交换链表中的节点.cpp

---
+++ 链表/24. 两两交换链表中的节点.cpp
@@ -0,0 +1,17 @@
+24. 两两交换链表中的节点
+
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+=========================================
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode *t = head->next;
+        head->next = swapPairs(head->next->next);
+        t->next = head;
+        return t;
+    }
+};
