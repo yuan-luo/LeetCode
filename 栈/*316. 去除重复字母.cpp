f82b0b20fcf2d6de7799742b2d7ca52cca7a087d
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 09:03:42 +0800

    Create *316. 去除重复字母.cpp

---
+++ 栈/*316. 去除重复字母.cpp
@@ -0,0 +1,25 @@
+316. 去除重复字母
+
+给你一个字符串 s ，请你去除字符串中重复的字母，使得每个字母只出现一次。需保证 返回结果的字典序最小（要求不能打乱其他字符的相对位置）。
+
+注意：该题与 1081 https://leetcode-cn.com/problems/smallest-subsequence-of-distinct-characters 相同
+
+示例 1：
+
+输入：s = "bcabc"
+输出："abc"
+示例 2：
+
+输入：s = "cbacdcbc"
+输出："acdb"
+
+=================================================
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+
+    }
+};
+
+
