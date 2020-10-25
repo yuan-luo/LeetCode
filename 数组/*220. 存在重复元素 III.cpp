31e9775b1cfb7974cfbd56082ebe2b43c0b04176
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 11:24:32 +0800

    Create *220. 存在重复元素 III.cpp

---
+++ 数组/*220. 存在重复元素 III.cpp
@@ -0,0 +1,17 @@
+220. 存在重复元素 III
+
+https://leetcode-cn.com/problems/contains-duplicate-iii/
+
+在整数数组 nums 中，是否存在两个下标 i 和 j，使得 nums [i] 和 nums [j] 的差的绝对值小于等于 t ，且满足 i 和 j 的差的绝对值也小于等于 ķ 。
+
+如果存在则返回 true，不存在返回 false。
+
+================================
+
+
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+
+    }
+};
