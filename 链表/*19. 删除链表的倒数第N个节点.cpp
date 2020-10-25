4e364b7e8bd0df534c4958a7f1a9f17aa7414d2c
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 22:13:53 +0800

    Create *19. 删除链表的倒数第N个节点.cpp

---
+++ 链表/*19. 删除链表的倒数第N个节点.cpp
@@ -0,0 +1,26 @@
+19. 删除链表的倒数第N个节点
+
+给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+
+示例：
+
+给定一个链表: 1->2->3->4->5, 和 n = 2.
+
+当删除了倒数第二个节点后，链表变为 1->2->3->5.
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+    }
+};
