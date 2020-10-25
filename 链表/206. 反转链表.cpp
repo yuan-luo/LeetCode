963d17a9e4474eac79962d941a2a0f79ff04d35c
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 22:19:45 +0800

    Create 206. 反转链表.cpp

---
+++ 链表/206. 反转链表.cpp
@@ -0,0 +1,22 @@
+206. 反转链表
+
+反转一个单链表。
+
+示例:
+
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *pre = NULL, *cur = head;
+        while (cur) {
+            ListNode *tmp = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+};
