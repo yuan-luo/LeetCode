060afd3908f1fc1f3ac46f020a229e9b3ef8cf54
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 11:54:21 +0800

    Create *290. 单词规律.cpp

---
+++ 字符串/*290. 单词规律.cpp
@@ -0,0 +1,27 @@
+290. 单词规律
+
+给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+
+这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+
+示例1:
+
+输入: pattern = "abba", str = "dog cat cat dog"
+输出: true
+示例 2:
+
+输入:pattern = "abba", str = "dog cat cat fish"
+输出: false
+
+================================
+相似题目：
+同构字符串 简单
+单词规律 II 困难
+
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+
+    }
+};
