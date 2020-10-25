d717c0cac3763f87c9162a4647aeeb74d009ba2b
Yuan Luo <yuan.luo@hotmail.com>
2020-10-26 00:02:51 +0800

    Update *47. 全排列 II.cpp

--- 回溯算法/*47. 全排列 II.cpp
+++ 回溯算法/*47. 全排列 II.cpp
@@ -14,5 +14,32 @@ https://leetcode-cn.com/problems/permutations-ii/
   [2,1,1]
 ]
 
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> out;
+        sort(nums.begin(), nums.end());
+        vector<int> visited(nums.size(), 0); // visited or not
+        helper(nums, ans, out, visited, 0);
+        return ans;
+    }
 
+    void helper(vector<int>& nums, vector<vector<int>>& ans, vector<int>& out, vector<int>& visited, int level) {
+        if (level >= nums.size()) ans.push_back(out);
+        else {
+            for (int i = 0; i < nums.size(); ++i) {
+                if (visited[i] == 0) {
+                    if (i > 0 && nums[i] == nums[i - 1] && visited[i - 1] == 0) continue;
+                    visited[i] = 1;
+                    out.push_back(nums[i]);
+                    helper(nums, ans, out, visited, level + 1);
+                    out.pop_back();
+                    visited[i] = 0;
+                }
+            }
+        }
+    }
+
+};
 
