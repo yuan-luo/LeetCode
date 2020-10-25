6eff2e513695d6d46ae19e4a5c6b3995dcafeef4
Yuan Luo <yuan.luo@hotmail.com>
2020-10-25 22:17:05 +0800

    Create *025. K 个一组翻转链表.cpp

---
+++ 链表/*025. K 个一组翻转链表.cpp
@@ -0,0 +1,38 @@
+25. K 个一组翻转链表
+
+给你一个链表，每 k 个节点一组进行翻转，请你返回翻转后的链表。
+
+k 是一个正整数，它的值小于或等于链表的长度。
+
+如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+
+ 
+
+示例：
+
+给你这个链表：1->2->3->4->5
+
+当 k = 2 时，应当返回: 2->1->4->3->5
+
+当 k = 3 时，应当返回: 3->2->1->4->5
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        
+    }
+
+    ListNode *reverseList(ListNode* head) {
+        
+    }
+};
