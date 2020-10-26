45a92f6c96468ecabd96527659265e2bf8be0a8f
Yuan Luo <yuan.luo@hotmail.com>
2020-10-26 10:35:14 +0800

    Update 173. 二叉搜索树迭代器.cpp

--- 二叉树/173. 二叉搜索树迭代器.cpp
+++ 二叉树/173. 二叉搜索树迭代器.cpp
@@ -1,3 +1,32 @@
 173. 二叉搜索树迭代器
 
+利用递归中序遍历。
+
+class BSTIterator {
+public:
+    vector<int> nodes;
+    int index;
+    BSTIterator(TreeNode* root) {
+        index = -1;
+        inorder(root);
+    }
+
+    void inorder(TreeNode* root) {
+        if (root == nullptr) return;
+        inorder(root->left);
+        nodes.push_back(root->val);
+        inorder(root->right);
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        return nodes[++index];
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return index + 1 < nodes.size();
+    }
+    
+};
 
