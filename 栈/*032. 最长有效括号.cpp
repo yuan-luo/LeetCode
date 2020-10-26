f4e68eafab26ddb75ea3958fad39d65d334aa304
Yuan Luo <yuan.luo@hotmail.com>
2020-10-26 23:37:01 +0800

    Update *032. 最长有效括号.cpp

--- 栈/*032. 最长有效括号.cpp
+++ 栈/*032. 最长有效括号.cpp
@@ -13,6 +13,28 @@
 输出: 4
 解释: 最长有效括号子串为 "()()"
 
+解法一：
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int ans = 0;
+        stack<int> st;
+        st.push(-1);
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') st.push(i);
+            else {
+                st.pop();
+                if (st.empty()) st.push(i);
+                else ans = max(ans, i - st.top());
+            }
+        }
+        return ans;
+    }
+};
+
+解法二：
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
