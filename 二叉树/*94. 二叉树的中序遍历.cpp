bd7a22301991f73f124fb4d462e7908d0fa1ff44
Yuan Luo <yuan.luo@hotmail.com>
2020-10-26 10:38:20 +0800

    Create *94. 二叉树的中序遍历.cpp

---
+++ 二叉树/*94. 二叉树的中序遍历.cpp
@@ -0,0 +1,20 @@
+94. 二叉树的中序遍历
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> s;
+        TreeNode *p = root;
+        while (p || !s.empty()) {
+            while (p) {
+                s.push(p);
+                p = p->left;
+            }
+            p = s.top(); s.pop();
+            res.push_back(p->val);
+            p = p->right;
+        }
+        return res;
+    }
+};
