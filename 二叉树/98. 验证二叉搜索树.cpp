3b5a4cc7ae65160d5c6deabab0aa199b24642f30
Yuan Luo <yuan.luo@hotmail.com>
2020-10-26 13:42:17 +0800

    Create 98. 验证二叉搜索树.cpp

---
+++ 二叉树/98. 验证二叉搜索树.cpp
@@ -0,0 +1,22 @@
+98. 验证二叉搜索树
+
+给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+
+假设一个二叉搜索树具有如下特征：
+
+节点的左子树只包含小于当前节点的数。
+节点的右子树只包含大于当前节点的数。
+所有左子树和右子树自身必须也是二叉搜索树。
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return isValidBST(root, LONG_MIN, LONG_MAX);
+    }
+
+    bool isValidBST(TreeNode* root, long lower, long upper) {
+        if (!root) return true;
+        if (root->val <= lower || root->val >= upper) return false;
+        return isValidBST(root->left, lower, root->val) && isValidBST(root->right, root->val, upper);
+    }
+};
