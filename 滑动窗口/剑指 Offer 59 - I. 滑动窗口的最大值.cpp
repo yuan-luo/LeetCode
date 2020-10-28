37482dabb5ea6b059b885bd124f5a67eaa104210
Yuan Luo <yuan.luo@hotmail.com>
2020-10-28 12:46:31 +0800

    Update 剑指 Offer 59 - I. 滑动窗口的最大值.cpp

--- 滑动窗口/剑指 Offer 59 - I. 滑动窗口的最大值.cpp
+++ 滑动窗口/剑指 Offer 59 - I. 滑动窗口的最大值.cpp
@@ -19,7 +19,11 @@ https://leetcode-cn.com/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/
  1  3  -1  -3 [5  3  6] 7       6
  1  3  -1  -3  5 [3  6  7]      7
 
+本题的关键是设计一个deque，记录当前滑动窗口大小为K的从大到小的数。queue中的每一个数记录的是该数字在nums数组中的下标。
+当queue头的数字下标滑出窗口范围的时候把它弹出。q.front() == i - k
 
+任何一个数字进入deque之前，先把deque尾部小于自己的数字弹出，以保证deque中的数都大于自己。
+这样deque头部就是滑动窗口最大数字。每次把dequeue头的数字加入结果。
 
 class Solution {
 public:
