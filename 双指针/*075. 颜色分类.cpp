2c0e7b20d1cb02be3c1bdbcd0fd1962dd9cfdb8d
Yuan Luo <yuan.luo@hotmail.com>
2020-10-28 22:09:37 +0800

    Update and rename 75. 颜色分类.cpp to *075. 颜色分类.cpp

--- 双指针/*075. 颜色分类.cpp
+++ 双指针/*075. 颜色分类.cpp
@@ -19,17 +19,10 @@ https://leetcode-cn.com/problems/sort-colors/
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int p0 = 0, p2 = nums.size() - 1;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            while (i <= p2 && nums[i] == 2) {
-                swap(nums[i], nums[p2]);
-                --p2;
-            }
-            if (nums[i] == 0) {
-                swap(nums[i], nums[p0]);
-                ++p0;
-            }
+        int l = 0, r = nums.size() - 1;
+        for (int i = 0; i <= r; ++i) {
+            if (nums[i] == 0) swap(nums[i], nums[l++]);
+            else if (nums[i] == 2) swap(nums[i--], nums[r--]);
         }
     }
 };
