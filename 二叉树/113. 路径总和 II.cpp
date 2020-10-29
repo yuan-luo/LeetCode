e9865c358d8ccafcfbdd970c485b488b9228ab37
Yuan Luo <yuan.luo@hotmail.com>
2020-10-29 09:59:02 +0800

    Create 113. 路径总和 II.cpp

---
+++ 二叉树/113. 路径总和 II.cpp
@@ -0,0 +1,27 @@
+113. 路径总和 II
+
+给定一个二叉树和一个目标和，找到所有从根节点到叶子节点路径总和等于给定目标和的路径。
+
+说明: 叶子节点是指没有子节点的节点。
+
+class Solution {
+public:
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> res;
+        vector<int> out;
+        helper(root, sum, res, out);
+        return res;
+    }
+
+    void helper(TreeNode* root, int sum, vector<vector<int>>& res, vector<int>& out) {
+        if (root == nullptr) return;
+        out.push_back(root->val);
+        sum -= root->val;
+        if (sum == 0 && root->left == nullptr && root->right == nullptr)
+            res.push_back(out);
+        helper(root->left, sum, res, out);
+        helper(root->right, sum, res, out);
+        out.pop_back();
+    }
+
+};
