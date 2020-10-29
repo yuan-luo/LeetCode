e7a9df35aedbaa39724862cebac89e04dcb91afc
Yuan Luo <yuan.luo@hotmail.com>
2020-10-29 23:02:14 +0800

    Update Offer 59 - II. 队列的最大值.cpp

--- 队列/Offer 59 - II. 队列的最大值.cpp
+++ 队列/Offer 59 - II. 队列的最大值.cpp
@@ -1,3 +1,25 @@
+请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+
+若队列为空，pop_front 和 max_value 需要返回 -1
+
+示例 1：
+
+输入: 
+["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+[[],[1],[2],[],[],[]]
+输出: [null,null,null,2,1,2]
+示例 2：
+
+输入: 
+["MaxQueue","pop_front","max_value"]
+[[],[],[]]
+输出: [null,-1,-1]
+
+题解：
+
+设计一个queue和一个deque。
+deque永远记录当前queue中的最大数。
+
 class MaxQueue {
 public:
     MaxQueue() {
