47b8d48ba298a5697a356887da3f4e151a2ad730
Yuan Luo <yuan.luo@hotmail.com>
2020-10-29 21:34:03 +0800

    Update 231. 2的幂.cpp

--- 数学/231. 2的幂.cpp
+++ 数学/231. 2的幂.cpp
@@ -15,3 +15,13 @@ public:
         return (x & (x - 1)) == 0;
     }
 };
+
+方法二：
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0) return false;
+        long x = n;
+        return (x & (-x)) == x;
+    }
+ };
