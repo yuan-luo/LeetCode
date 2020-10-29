645bf882724c245cf9d4d654a8bbb782e9a9a6c8
Yuan Luo <yuan.luo@hotmail.com>
2020-10-29 13:44:00 +0800

    Create 78. 子集.cpp

---
+++ 回溯算法/78. 子集.cpp
@@ -0,0 +1,37 @@
+78. 子集
+
+给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+说明：解集不能包含重复的子集。
+
+输入: nums = [1,2,3]
+输出:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> out;
+        sort(nums.begin(), nums.end());
+        helper(ans, out, nums, 0);
+        return ans;
+    }
+
+    void helper(vector<vector<int>>& ans, vector<int>& out, vector<int>& nums, int level) {
+        ans.push_back(out);
+        for (int i = level; i < nums.size(); ++i) {
+            out.push_back(nums[i]);
+            helper(ans, out, nums, i + 1);
+            out.pop_back();
+        }
+    }
+};
