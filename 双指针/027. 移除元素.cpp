cf2158d693e97a5974edd9c8d5af17ea82214124
Yuan Luo <yuan.luo@hotmail.com>
2020-10-31 20:03:48 +0800

    Update and rename *27. 移除元素.cpp to 027. 移除元素.cpp

--- 双指针/027. 移除元素.cpp
+++ 双指针/027. 移除元素.cpp
@@ -25,3 +25,16 @@ https://leetcode-cn.com/problems/remove-element/
  
 =============================================
 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                ++i;
+            }
+        }
+        return i;
+    }
+};
