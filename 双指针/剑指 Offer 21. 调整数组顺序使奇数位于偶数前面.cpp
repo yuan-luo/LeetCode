67bec908c1f5c60616d4e5afdf4db37b8d8a3285
Yuan Luo <yuan.luo@hotmail.com>
2020-10-31 19:49:12 +0800

    Create 剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.cpp

---
+++ 双指针/剑指 Offer 21. 调整数组顺序使奇数位于偶数前面.cpp
@@ -0,0 +1,20 @@
+剑指 Offer 21. 调整数组顺序使奇数位于偶数前面
+
+输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+
+class Solution {
+public:
+    vector<int> exchange(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            while (left < right && nums[left] % 2 == 1) {
+                ++left;
+            }
+            while (left < right && nums[right] % 2 == 0) {
+                --right;
+            }
+            swap(nums[left], nums[right]);
+        }
+        return nums;
+    }
+};  
