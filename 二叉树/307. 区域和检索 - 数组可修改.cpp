ce46915ab5e9d715e4c0c21264360dfa5da05c62
Yuan Luo <yuan.luo@hotmail.com>
2020-11-01 20:48:08 +0800

    Create 307. 区域和检索 - 数组可修改.cpp

---
+++ 二叉树/307. 区域和检索 - 数组可修改.cpp
@@ -0,0 +1,82 @@
+307. 区域和检索 - 数组可修改
+
+给定一个整数数组  nums，求出数组从索引 i 到 j  (i ≤ j) 范围内元素的总和，包含 i,  j 两点。
+
+update(i, val) 函数可以通过将下标为 i 的数值更新为 val，从而对数列进行修改。
+
+示例:
+
+Given nums = [1, 3, 5]
+
+sumRange(0, 2) -> 9
+update(1, 2)
+sumRange(0, 2) -> 8
+说明:
+
+数组仅可以在 update 函数下进行修改。
+你可以假设 update 函数与 sumRange 函数的调用次数是均匀分布的。
+
+题解：
+
+此题用线段树。
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) {
+        if (nums.size() > 0) {
+            n = nums.size();
+            tree.resize(n * 2);
+            buildTree(nums);
+        }
+    }
+
+    void buildTree(vector<int>& nums) {
+        for (int i = n, j = 0; i < 2 * n; ++i, ++j)
+            tree[i] = nums[j];
+        for (int i = n - 1; i > 0; --i)
+            tree[i] = tree[i * 2] + tree[i * 2 + 1];
+    }
+
+    
+    void update(int pos, int val) {
+        pos += n;
+        tree[pos] = val;
+        while (pos > 0) {
+            int left = pos;
+            int right = pos;
+            if (pos % 2 == 0) { 
+                right = pos + 1;
+            } else {
+                left = pos - 1;
+            }
+            // parent is updated after child is updated
+            tree[pos / 2] = tree[left] + tree[right];
+            pos /= 2;
+        }
+    }
+    
+    int sumRange(int l, int r) {
+        // get leaf with value 'l'
+        l += n;
+        // get leaf with value 'r'
+        r += n;
+        int sum = 0;
+        while (l <= r) {
+            if ((l % 2) == 1) {
+                sum += tree[l];
+                l++;
+            }
+            if ((r % 2) == 0) {
+                sum += tree[r];
+                r--;
+            }
+            l /= 2;
+            r /= 2;
+        }
+        return sum;
+    }
+
+private:
+    vector<int> tree;
+    int n;
+};
