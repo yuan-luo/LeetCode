4c720a26508d0e40ccc270744ff5b82f443870e3
Yuan Luo <yuan.luo@hotmail.com>
2020-11-01 13:45:50 +0800

    Create 剑指 Offer 65. 不用加减乘除做加法.cpp

---
+++ 位运算/剑指 Offer 65. 不用加减乘除做加法.cpp
@@ -0,0 +1,13 @@
+剑指 Offer 65. 不用加减乘除做加法
+
+class Solution {
+public:
+    int add(int a, int b) {
+        while (b != 0) {
+            int c = (unsigned int) (a & b) << 1; // c是进位
+            a = a ^ b; // 非进位和
+            b = c; 
+        }
+        return a;
+    }
+};  
