833e00279a6a99fb0f4b6de4d0bc416c187ca405
Yuan Luo <yuan.luo@hotmail.com>
2020-11-03 00:05:23 +0800

    Update 035. 搜索插入位置.cpp

--- 二分查找/035. 搜索插入位置.cpp
+++ 二分查找/035. 搜索插入位置.cpp
@@ -40,4 +40,33 @@ public:
     }
 };
 
+解法二：
 
+class Solution {
+public:
+    int searchInsert(vector<int> &nums, int target) {
+        int size = nums.size();
+        if (size == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        // 因为有可能数组的最后一个元素的位置的下一个是我们要找的，故右边界是 len
+        int right = size;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            // 小于 target 的元素一定不是解
+            if (nums[mid] < target) {
+                // 下一轮搜索的区间是 [mid + 1, right]
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
+
+作者：liweiwei1419
+链接：https://leetcode-cn.com/problems/search-insert-position/solution/te-bie-hao-yong-de-er-fen-cha-fa-fa-mo-ban-python-/
