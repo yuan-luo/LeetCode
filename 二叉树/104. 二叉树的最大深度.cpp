0954120a1c6fee368d71a4b9af896ad41ba83b66
Yuan Luo <yuan.luo@hotmail.com>
2020-11-05 20:13:40 +0800

    Create 104. 二叉树的最大深度.cpp

---
+++ 二叉树/104. 二叉树的最大深度.cpp
@@ -0,0 +1,17 @@
+104. 二叉树的最大深度
+
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1 + max(left, right);
+    }
+};
