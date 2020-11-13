a7dc8c176f0728ec936ec9349100b0b3843b4db9
Yuan Luo <yuan.luo@hotmail.com>
2020-11-13 17:08:14 +0800

    Update *328. 奇偶链表.cpp

--- 链表/*328. 奇偶链表.cpp
+++ 链表/*328. 奇偶链表.cpp
@@ -19,20 +19,20 @@
 
 ==========================================
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        
+        if (head == nullptr) return nullptr;
+        ListNode* odd = head;
+        ListNode* even = odd->next;
+        ListNode* evenHead = odd->next;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            even->next = even->next->next;
+            odd = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
     }
 };
-
