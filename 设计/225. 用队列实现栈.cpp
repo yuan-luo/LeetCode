ced2e49f7e405bd2425e407c1ca1b78a19192b5f
Yuan Luo <yuan.luo@hotmail.com>
2020-11-15 10:50:31 +0800

    Update 225. 用队列实现栈.cpp

--- 设计/225. 用队列实现栈.cpp
+++ 设计/225. 用队列实现栈.cpp
@@ -14,6 +14,15 @@ boolean empty() 如果队列为空，返回 true ；否则，返回 false
 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
 
+题解思路：
+
+这道题的关键是在pop的时候要把queue前面n - 1个数字依次弹出并加到queue队尾去。
+这样在queue首的就是最加入的数字。
+比如：
+1 2 3 4 5 要pop出5，那么把1，2，3，4依次插入队尾。
+5 1 2 3 4 然后把5再pop出去。
+要得到top的算法也是类似。
+
 class MyStack {
 public:
     /** Initialize your data structure here. */
