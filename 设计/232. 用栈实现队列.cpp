5b31351d53440a98e08c66f9a12d39a17d86cea0
Yuan Luo <yuan.luo@hotmail.com>
2020-11-15 11:40:27 +0800

    Update 232. 用栈实现队列.cpp

--- 设计/232. 用栈实现队列.cpp
+++ 设计/232. 用栈实现队列.cpp
@@ -14,6 +14,10 @@ boolean empty() 如果队列为空，返回 true ；否则，返回 false
 你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
 
+解题思路：
+
+这道题要注意pop和peek的时候要检查st2是否为空，只有为空的情况下才能把st1的数字挪过来。
+
 class MyQueue {
 public:
     /** Initialize your data structure here. */
