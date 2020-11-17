b69d6f7ef7c00e7af13a3a7ca1584559bebf3d40
Yuan Luo <yuan.luo@hotmail.com>
2020-11-17 22:25:27 +0800

    Create 518. 零钱兑换 II.cpp

---
+++ 动态规划/518. 零钱兑换 II.cpp
@@ -0,0 +1,27 @@
+518. 零钱兑换 II
+
+给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。 
+
+示例 1:
+
+输入: amount = 5, coins = [1, 2, 5]
+输出: 4
+解释: 有四种方式可以凑成总金额:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1, 0);
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
