9e4bbf59d1e1a85c9cddff1882266861b33923b2
Yuan Luo <yuan.luo@hotmail.com>
2020-11-18 20:24:03 +0800

    Create 055. 跳跃游戏.cpp

---
+++ 数组/055. 跳跃游戏.cpp
@@ -0,0 +1,19 @@
+55. 跳跃游戏
+
+给定一个非负整数数组，你最初位于数组的第一个位置。
+
+数组中的每个元素代表你在该位置可以跳跃的最大长度。
+
+判断你是否能够到达最后一个位置。
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = nums[0];
+        for (int i = 0; i <= reach; ++i) {
+            reach = max(reach, i + nums[i]);
+            if (reach >= nums.size() - 1) return true;
+        }
+        return false;
+    }
+};
