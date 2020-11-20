fb76bbf1ec25a9d21c52a7fb9797f456746f675d
Yuan Luo <yuan.luo@hotmail.com>
2020-11-20 16:12:02 +0800

    Update *147. 对链表进行插入排序.cpp

--- 链表/*147. 对链表进行插入排序.cpp
+++ 链表/*147. 对链表进行插入排序.cpp
@@ -2,3 +2,22 @@
 
 对链表进行插入排序。
 
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(0);
+        ListNode* cur = head;
+        ListNode* pre = &dummy;
+        ListNode* next = nullptr;
+        while (cur != nullptr) {
+            next = cur->next;
+            while (pre->next != nullptr && pre->next->val <= cur->val) pre = pre->next;
+
+            cur->next = pre->next;
+            pre->next = cur;
+            pre = &dummy;
+            cur = next;
+        }
+        return dummy.next;
+    }
+};
