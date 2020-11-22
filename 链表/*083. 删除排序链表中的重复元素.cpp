289f7a681fbd46de70a1cd16bee9c05c57cafbe4
Yuan Luo <yuan.luo@hotmail.com>
2020-11-22 15:54:45 +0800

    Update *083. 删除排序链表中的重复元素.cpp

--- 链表/*083. 删除排序链表中的重复元素.cpp
+++ 链表/*083. 删除排序链表中的重复元素.cpp
@@ -15,17 +15,15 @@ https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
 
 ======================
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        
+        if (head == nullptr) return nullptr;
+        ListNode* cur = head;
+        while (cur != nullptr && cur->next != nullptr) {
+            if (cur->next->val == cur->val) cur->next = cur->next->next;
+            else cur = cur->next;
+        }
+        return head;
     }
 };
