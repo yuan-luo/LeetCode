993c66fa0fb32aa56812b35d1ebf528cb8823a2d
Yuan Luo <yuan.luo@hotmail.com>
2020-11-22 11:15:32 +0800

    Update 225. 用队列实现栈.cpp

--- 设计/225. 用队列实现栈.cpp
+++ 设计/225. 用队列实现栈.cpp
@@ -1,18 +1,16 @@
 225. 用队列实现栈
 
-请你仅使用两个栈实现先入先出队列。队列应当支持一般队列的支持的所有操作（push、pop、peek、empty）：
+使用队列实现栈的下列操作：
 
-实现 MyQueue 类：
+push(x) -- 元素 x 入栈
+pop() -- 移除栈顶元素
+top() -- 获取栈顶元素
+empty() -- 返回栈是否为空
+注意:
 
-void push(int x) 将元素 x 推到队列的末尾
-int pop() 从队列的开头移除并返回元素
-int peek() 返回队列开头的元素
-boolean empty() 如果队列为空，返回 true ；否则，返回 false
- 
-说明：
-
-你只能使用标准的栈操作 —— 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
-你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+你只能使用队列的基本操作-- 也就是 push to back, peek/pop from front, size, 和 is empty 这些操作是合法的。
+你所使用的语言也许不支持队列。 你可以使用 list 或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+你可以假设所有操作都是有效的（例如, 对一个空的栈不会调用 pop 或者 top 操作）。
 
 题解思路：
 
