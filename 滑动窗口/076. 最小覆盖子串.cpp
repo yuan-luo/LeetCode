3edf780d6ba80754b4167670e9282ca2d6c2511b
Yuan Luo <yuan.luo@hotmail.com>
2020-11-22 09:46:08 +0800

    Update 076. 最小覆盖子串.cpp

--- 滑动窗口/076. 最小覆盖子串.cpp
+++ 滑动窗口/076. 最小覆盖子串.cpp
@@ -3,18 +3,18 @@
 给你一个字符串 S、一个字符串 T 。请你设计一种算法，可以在 O(n) 的时间复杂度内，从字符串 S 里面找出：包含 T 所有字符的最小子串。
 
 示例：
-
 输入：S = "ADOBECODEBANC", T = "ABC"
 输出："BANC"
 
-相似题目：
-串联所有单词的子串 困难
-长度最小的子数组 中等
-滑动窗口最大值 困难
-字符串的排列 中等
-最小窗口子序列 困难
+这道题和第3题有相似的地方。
+https://github.com/yuan-luo/LeetCode/blob/master/%E5%AD%97%E7%AC%A6%E4%B8%B2/003.%20%E6%97%A0%E9%87%8D%E5%A4%8D%E5%AD%97%E7%AC%A6%E7%9A%84%E6%9C%80%E9%95%BF%E5%AD%90%E4%B8%B2.cpp
 
-这道题和第3题有很多相似的地方。
+3. 无重复字符的最长子串
+https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+
+
 
 
 
