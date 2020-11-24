f659cfe5b814cb1b646510da4434bfdb578bff50
Yuan Luo <yuan.luo@hotmail.com>
2020-11-24 20:40:06 +0800

    Create 217. 存在重复元素.cpp

---
+++ 数组/217. 存在重复元素.cpp
@@ -0,0 +1,30 @@
+217. 存在重复元素
+
+给定一个整数数组，判断是否存在重复元素。
+
+如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+示例 1:
+
+输入: [1,2,3,1]
+输出: true
+示例 2:
+
+输入: [1,2,3,4]
+输出: false
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m.find(nums[i]) == m.end()) ++m[nums[i]];
+            else return true;
+        } 
+        return false;       
+    }
+};
