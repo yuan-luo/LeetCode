94a08506f6eb59d427ed104a3276ed41213204c9
Yuan Luo <yuan.luo@hotmail.com>
2020-11-24 17:29:16 +0800

    Update *470. 用 Rand7() 实现 Rand10().cpp

--- 数学/*470. 用 Rand7() 实现 Rand10().cpp
+++ 数学/*470. 用 Rand7() 实现 Rand10().cpp
@@ -1,3 +1,15 @@
 470. 用 Rand7() 实现 Rand10()
 
+已有方法 rand7 可生成 1 到 7 范围内的均匀随机整数，试写一个方法 rand10 生成 1 到 10 范围内的均匀随机整数。
 
+不要使用系统的 Math.random() 方法。
+
+class Solution {
+public:
+    int rand10() {
+        while (true) {
+            int num = (rand7() - 1) * 7 + rand7(); // 等概率生成[1,49]范围的随机数
+            if (num <= 40) return num % 10 + 1; // 拒绝采样，并返回[1,10]范围的随机数
+        }
+    }
+};
