2e329c753a772225f9c942a4d2924d2ee51fa105
Yuan Luo <yuan.luo@hotmail.com>
2020-11-24 15:35:32 +0800

    Update 面试题 04.06. 后继者.cpp

--- 二叉树/面试题 04.06. 后继者.cpp
+++ 二叉树/面试题 04.06. 后继者.cpp
@@ -30,3 +30,27 @@ public:
         return nullptr;
     }
 };
+
+针对BST树的递归解法非常精妙：
+https://leetcode-cn.com/problems/successor-lcci/solution/zhong-xu-bian-li-de-xia-yi-ge-yuan-su-5da-jie-fa-h/
+
+首先本题中的二叉树还是个二叉搜索树，也就是中序遍历是单调递增的，所以我们可以利用这个性质来简化查找过程。
+
+如果结点 p 的值大于等于 root 的值，说明 p 的后继结点在 root 右子树中，那么就递归到右子树中查找。
+如果结点 p 的值小于 root 的值，说明 p 在 root 左子树中，而它的后继结点有两种可能，要么也在左子树中，要么就是 root：
+如果左子树中找到了后继结点，那就直接返回答案。
+如果左子树中没有找到后继结点，那就说明 p 的右儿子为空，那么 root 就是它的后继结点。
+
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        if (root == NULL || p == NULL) return NULL;
+        if (p->val >= root->val) {
+            return inorderSuccessor(root->right, p);
+        } else {
+            TreeNode* left = inorderSuccessor(root->left, p);
+            return left ? left : root;
+        }
+    }
+};
+
