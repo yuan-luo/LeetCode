1db364c842e392eb6f9dfee32b25a468635f3746
Yuan Luo <yuan.luo@hotmail.com>
2020-11-25 23:41:01 +0800

    Create 162. 寻找峰值.cpp

---
+++ 二分查找/162. 寻找峰值.cpp
@@ -0,0 +1,22 @@
+162. 寻找峰值
+
+峰值元素是指其值大于左右相邻值的元素。
+
+给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+
+数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+
+你可以假设 nums[-1] = nums[n] = -∞。
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+};
