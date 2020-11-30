2d33ddec964446fc9a4e23249fd08883e0c6d266
Yuan Luo <yuan.luo@hotmail.com>
2020-11-30 20:41:53 +0800

    Update 146. LRU缓存机制.cpp

--- 设计/146. LRU缓存机制.cpp
+++ 设计/146. LRU缓存机制.cpp
@@ -2,6 +2,16 @@
 
 https://leetcode-cn.com/problems/lru-cache/
 
+解题思路：
+
+我们用一个cachelist记录这个key/value对的list。用一个cachemap记录某个key的list pair对的iterator游标。
+这样从map中就能直接定位找到list中pair的地址。
+每次get的时候我们直接通过key找到pair在cachemap中的位置，然后删除它，并在list头加上这个pair对，然后把地址赋给cachemap。
+每次put的时候我们先通过key找到pair在cachemap中的位置，如果有就删除它。然后在list头加上这个pair对，让后把地址赋给cachemap，这里多一个删除溢出的值的逻辑，这里删除list最后一个元素。
+
+list<pair<int, int>> cacheList; // key/value pair list
+unordered_map<int, list<pair<int, int>>::iterator> cacheMap; // key : cacheList iterator
+
 class LRUCache {
 public:
     LRUCache(int capacity): cap(capacity) {
