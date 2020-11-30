b22c98fea0f643e5af6691ae17b6bfbdd12e0c56
Yuan Luo <yuan.luo@hotmail.com>
2020-11-30 19:34:20 +0800

    Create 341. 扁平化嵌套列表迭代器.cpp

---
+++ 设计/341. 扁平化嵌套列表迭代器.cpp
@@ -0,0 +1,65 @@
+341. 扁平化嵌套列表迭代器
+
+给你一个嵌套的整型列表。请你设计一个迭代器，使其能够遍历这个整型列表中的所有整数。
+
+列表中的每一项或者为一个整数，或者是另一个列表。其中列表的元素也可能是整数或是其他列表。
+
+示例 1:
+
+输入: [[1,1],2,[1,1]]
+输出: [1,1,2,1,1]
+解释: 通过重复调用 next 直到 hasNext 返回 false，next 返回的元素的顺序应该是: [1,1,2,1,1]。
+
+
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        make_queue(nestedList);
+    }
+    
+    int next() {
+        int val;
+        if (!q.empty()) {
+            val = q.front().getInteger();
+            q.pop();
+        }
+        return val;
+    }
+    
+    bool hasNext() {
+        return !q.empty();
+    }
+
+    void make_queue(vector<NestedInteger> &nestedList) {
+        for (auto item : nestedList) {
+            if (item.isInteger()) q.push(item.getInteger());
+            else make_queue(item.getList());
+        }
+    }
+private:
+    queue<NestedInteger> q;
+};
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
