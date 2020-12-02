8e4526c3c0063c2fe85e88330f971b9a4967518d
Yuan Luo <yuan.luo@hotmail.com>
2020-12-02 21:35:31 +0800

    Update 946. 验证栈序列.cpp

--- 栈/946. 验证栈序列.cpp
+++ 栈/946. 验证栈序列.cpp
@@ -1,17 +1,13 @@
 946. 验证栈序列
 
 此题和剑指 Offer 31. 栈的压入、弹出序列雷同
-
 https://leetcode-cn.com/problems/validate-stack-sequences/
 
 给定 pushed 和 popped 两个序列，每个序列中的 值都不重复，只有当它们可能是在最初空栈上进行的推入 push 和弹出 pop 操作序列的结果时，返回 true；否则，返回 false 。
 
 示例 1：
-
 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
 输出：true
-
-===================================================
 
 题解:
 
