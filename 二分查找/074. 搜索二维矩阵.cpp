05b118c566a635fa0156aa85bae9cb1adcd90ccc
Yuan Luo <yuan.luo@hotmail.com>
2020-12-03 14:13:29 +0800

    Update 074. 搜索二维矩阵.cpp

--- 二分查找/074. 搜索二维矩阵.cpp
+++ 二分查找/074. 搜索二维矩阵.cpp
@@ -14,3 +14,19 @@
 输入：matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,50]], target = 13
 输出：false
 
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.empty() || matrix[0].empty()) return false;
+        int m = matrix.size(), n = matrix[0].size();
+        int left = 0, right = m * n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int pivotVal = matrix[mid / n][mid % n];
+            if (pivotVal == target) return true;
+            else if (pivotVal < target) left = mid + 1;
+            else right = mid - 1;
+        }
+        return false;
+    }
+};
