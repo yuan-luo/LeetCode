10fc566c361df275bb78ad5d4ab0bb2baf038a07
Yuan Luo <yuan.luo@hotmail.com>
2020-12-03 23:15:59 +0800

    Create 084. 柱状图中最大的矩形.cpp

---
+++ 栈/084. 柱状图中最大的矩形.cpp
@@ -0,0 +1,32 @@
+84. 柱状图中最大的矩形
+
+给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+
+求在该柱状图中，能够勾勒出来的矩形的最大面积。
+
+以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+
+示例:
+
+输入: [2,1,5,6,2,3]
+输出: 10
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int res = 0;
+        stack<int> st;
+        heights.push_back(0); // 注意这里先给heights队尾加入一个元素，以保证会处理最后一个高度值。
+        for (int i = 0; i < heights.size(); ++i) {
+            if (st.empty() || heights[st.top()] < heights[i]) {
+                st.push(i);                
+            } else {
+                int cur = st.top(); st.pop();
+                res = max(res, heights[cur] * (st.empty() ? i : (i - st.top() - 1)));
+                --i;                
+            }
+        }   
+        return res;
+    }
+};
