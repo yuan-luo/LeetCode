11922f8d7b3cc6b484477a6acd02c398871592a1
Yuan Luo <yuan.luo@hotmail.com>
2020-12-03 21:26:35 +0800

    Update *150. 逆波兰表达式求值.cpp

--- 栈/*150. 逆波兰表达式求值.cpp
+++ 栈/*150. 逆波兰表达式求值.cpp
@@ -14,11 +14,13 @@
 输入: ["2", "1", "+", "3", "*"]
 输出: 9
 解释: 该算式转化为常见的中缀算术表达式为：((2 + 1) * 3) = 9
+
 示例 2：
 
 输入: ["4", "13", "5", "/", "+"]
 输出: 6
 解释: 该算式转化为常见的中缀算术表达式为：(4 + (13 / 5)) = 6
+
 示例 3：
 
 输入: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
@@ -33,3 +35,23 @@
 = 17 + 5
 = 22
 
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        if (tokens.size() == 1) return stoi(tokens[0]);
+        stack<int> stk;
+        for (int i = 0; i < tokens.size(); ++i) {
+            if (tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                stk.push(stoi(tokens[i]));
+            } else {
+                int num1 = stk.top(); stk.pop();
+                int num2 = stk.top(); stk.pop();
+                if (tokens[i] == "+") stk.push(num2 + num1);
+                if (tokens[i] == "-") stk.push(num2 - num1);
+                if (tokens[i] == "*") stk.push(num2 * num1);
+                if (tokens[i] == "/") stk.push(num2 / num1);
+            }
+        }
+        return stk.top();
+    }
+};
