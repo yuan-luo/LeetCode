1c5d41ce726184573370bc562db1bb4fedb84680
Yuan Luo <yuan.luo@hotmail.com>
2020-12-06 12:06:49 +0800

    Create 378. 有序矩阵中第K小的元素.cpp

---
+++ 二分查找/378. 有序矩阵中第K小的元素.cpp
@@ -0,0 +1,36 @@
+378. 有序矩阵中第K小的元素
+
+给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
+请注意，它是排序后的第 k 小元素，而不是第 k 个不同的元素。
+
+示例：
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+返回 13。
+
+题解：
+
+此题用小顶堆肯定能做，但是二分查找是本题考查的重点，也能让时间复杂度小于O(n^2)。
+先找到中点，然后每行比较。如果cnt累计值小于k，往后找。否则往前找。
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int left = matrix[0][0], right = matrix.back().back();
+        while (left < right) {
+            int mid = left + (right - left) / 2, cnt  = 0;
+            for (int i = 0; i < matrix.size(); ++i) {
+                cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+            }
+            if (cnt < k) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+};
