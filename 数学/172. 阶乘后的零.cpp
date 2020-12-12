7ff51e505d42dde3b4ada0f378f7be4f7addde8d
Yuan Luo <yuan.luo@hotmail.com>
2020-12-12 20:18:48 +0800

    Update 172. 阶乘后的零.cpp

--- 数学/172. 阶乘后的零.cpp
+++ 数学/172. 阶乘后的零.cpp
@@ -11,11 +11,11 @@ https://leetcode-cn.com/problems/factorial-trailing-zeroes/
 class Solution {
 public:
     int trailingZeroes(int n) {
-        int res = 0;
-        while (n > 0) {
-            res += n / 5;
+        int ans = 0;
+        while (n) {
+            ans += n / 5;
             n /= 5;
         }
-        return res;
+        return ans;
     }
 };
