844584f82707f3db4aa8ee24e270351f4c6b5358
Yuan Luo <yuan.luo@hotmail.com>
2020-12-13 17:23:58 +0800

    Update *075. 颜色分类.cpp

--- 双指针/*075. 颜色分类.cpp
+++ 双指针/*075. 颜色分类.cpp
@@ -44,10 +44,16 @@ public:
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int l = 0, r = nums.size() - 1;
-        for (int i = 0; i <= r; ++i) {
-            if (nums[i] == 0) swap(nums[i], nums[l++]);
-            else if (nums[i] == 2) swap(nums[i--], nums[r--]);
+        int p0 = 0, p2 = nums.size() - 1;
+        for (int i = 0; i <= p2; ++i) {
+            if (nums[i] == 0) {
+                swap(nums[i], nums[p0]);
+                p0++; 
+            }
+            if (nums[i] == 2) {
+                swap(nums[i], nums[p2]);
+                p2--; --i;
+            }
         }
     }
 };
