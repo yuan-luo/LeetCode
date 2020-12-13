619e20ce13b75374a52b51bfa1e48b579b224ded
Yuan Luo <yuan.luo@hotmail.com>
2020-12-13 12:14:01 +0800

    Update 303. 区域和检索 - 数组不可变.cpp

--- 动态规划/303. 区域和检索 - 数组不可变.cpp
+++ 动态规划/303. 区域和检索 - 数组不可变.cpp
@@ -1,14 +1,11 @@
 303. 区域和检索 - 数组不可变
 
 给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
-
 实现 NumArray 类：
-
 NumArray(int[] nums) 使用数组 nums 初始化对象
 int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
  
 示例：
-
 输入：
 ["NumArray", "sumRange", "sumRange", "sumRange"]
 [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
