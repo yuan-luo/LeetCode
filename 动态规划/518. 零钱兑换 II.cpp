8b4c85145eea20cee6971ad15c9a360eaf3d9688
Yuan Luo <yuan.luo@hotmail.com>
2020-12-14 22:57:17 +0800

    Update 518. 零钱兑换 II.cpp

--- 动态规划/518. 零钱兑换 II.cpp
+++ 动态规划/518. 零钱兑换 II.cpp
@@ -30,6 +30,15 @@ public:
 
 解法二：二维DP
 
+dp[i][j] : the number of combinations to make up amount j by using the first i types of coins
+State transition:
+
+not using the ith coin, only using the first i-1 coins to make up amount j, then we have dp[i-1][j] ways.
+using the ith coin, since we can use unlimited same coin, 
+we need to know how many ways to make up amount j - coins[i-1] by using first i coins(including ith), which is dp[i][j-coins[i-1]]
+
+Initialization: dp[i][0] = 1
+
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
