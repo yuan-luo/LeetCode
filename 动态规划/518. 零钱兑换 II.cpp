217f9a127256c3aca5921fa349065ee06b688b84
Yuan Luo <yuan.luo@hotmail.com>
2020-12-14 22:50:52 +0800

    Update 518. 零钱兑换 II.cpp

--- 动态规划/518. 零钱兑换 II.cpp
+++ 动态规划/518. 零钱兑换 II.cpp
@@ -12,6 +12,8 @@
 5=2+1+1+1
 5=1+1+1+1+1
 
+解法一：一维DP
+
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
@@ -23,5 +25,24 @@ public:
             }
         }
         return dp[amount];
+    }
+};
+
+解法二：二维DP
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        // dp[i][j]: i个硬币组成j金额
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
+        for (int i = 0; i <= n; ++i) dp[i][0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= amount; ++j) {
+                dp[i][j] += dp[i - 1][j];
+                if (j - coins[i - 1] >= 0) dp[i][j] += dp[i][j - coins[i - 1]];
+            }
+        }
+        return dp.back().back();
     }
 };
