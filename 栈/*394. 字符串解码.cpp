358dcaa85b3bc0276c9ea8bb0c5dfbce9773bf0e
Yuan Luo <yuan.luo@hotmail.com>
2020-12-20 10:59:27 +0800

    Update *394. 字符串解码.cpp

--- 栈/*394. 字符串解码.cpp
+++ 栈/*394. 字符串解码.cpp
@@ -27,3 +27,31 @@
 
 输入：s = "abc3[cd]xyz"
 输出："abccdcdcdxyz"
+
+
+class Solution {
+public:
+    string decodeString(const string& s, int& i) {
+        string res;
+        
+        while (i < s.length() && s[i] != ']') {
+            if (!isdigit(s[i])) res += s[i++];
+            else {
+                int n = 0;
+                while (i < s.length() && isdigit(s[i]))
+                    n = n * 10 + s[i++] - '0';
+                i++; // '['
+                string t = decodeString(s, i);
+                i++; // ']'
+                while (n-- > 0)
+                    res += t;
+            }
+        }
+        return res;
+    }
+
+    string decodeString(string s) {
+        int i = 0;
+        return decodeString(s, i);
+    }
+};
