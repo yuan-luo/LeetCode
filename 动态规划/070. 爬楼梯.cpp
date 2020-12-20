8d6494b686a69895960bce1717bbd627100b469f
Yuan Luo <yuan.luo@hotmail.com>
2020-12-20 11:24:28 +0800

    Update and rename 070. 爬楼梯 to 070. 爬楼梯.cpp

---
+++ 动态规划/070. 爬楼梯.cpp
@@ -0,0 +1,20 @@
+70. 爬楼梯
+
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int p = 0, q = 0, r = 1;
+        for (int i = 1; i <= n; ++i) {
+            p = q; 
+            q = r; 
+            r = p + q;
+        }
+        return r;
+    }
+};
