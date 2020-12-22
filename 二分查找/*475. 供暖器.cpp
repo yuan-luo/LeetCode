33df56c5f5e58ebfc0a3862cbd4ab150ce0850c4
Yuan Luo <yuan.luo@hotmail.com>
2020-12-22 21:48:08 +0800

    Update *475. 供暖器.cpp

--- 二分查找/*475. 供暖器.cpp
+++ 二分查找/*475. 供暖器.cpp
@@ -1,26 +1,31 @@
 475. 供暖器
 
 冬季已经来临。 你的任务是设计一个有固定加热半径的供暖器向所有房屋供暖。
-
 在加热器的加热半径范围内的每个房屋都可以获得供暖。
-
 现在，给出位于一条水平线上的房屋 houses 和供暖器 heaters 的位置，请你找出并返回可以覆盖所有房屋的最小加热半径。
-
 说明：所有供暖器都遵循你的半径标准，加热的半径也一样。
 
-示例 1:
+思路：
 
-输入: houses = [1,2,3], heaters = [2]
-输出: 1
-解释: 仅在位置2上有一个供暖器。如果我们将加热半径设为1，那么所有房屋就都能得到供暖。
+先将heaters排序。再依次处理每位house，利用二分查找找到每个house的右边第一个heater和左边第一个heater。
+然后每轮循环记录当前最大的heater半径。
 
-示例 2:
-
-输入: houses = [1,2,3,4], heaters = [1,4]
-输出: 1
-解释: 在位置1, 4上有两个供暖器。我们需要将加热半径设为1，这样所有房屋就都能得到供暖。
-
-示例 3：
-
-输入：houses = [1,5], heaters = [2]
-输出：3
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        int ans = 0, n = heaters.size();
+        sort(heaters.begin(), heaters.end());
+        for (auto house : houses) {
+            int left = 0, right = n;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (heaters[mid] < house) left = mid + 1;
+                else right = mid;
+            }
+            int inst1 = (right == n) ? INT_MAX : heaters[right] - house;
+            int inst2 = (right == 0) ? INT_MAX : house - heaters[right - 1];
+            ans = max(ans, min(inst1, inst2));
+        }
+        return ans;
+    }
+};
