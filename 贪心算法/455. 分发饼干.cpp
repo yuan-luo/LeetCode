542af6ba310c7ebac00812f1ad97b3646298fb9a
Yuan Luo <yuan.luo@hotmail.com>
2020-12-25 16:29:42 +0800

    Update 455. 分发饼干.cpp

--- 贪心算法/455. 分发饼干.cpp
+++ 贪心算法/455. 分发饼干.cpp
@@ -5,7 +5,7 @@
 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
 
- class Solution {
+class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int j = 0;
