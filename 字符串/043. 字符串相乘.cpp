9be65ea7487c9fc6d1bc762f5e322c7d8a17db75
Yuan Luo <yuan.luo@hotmail.com>
2021-01-05 22:23:20 +0800

    Create 043. 字符串相乘.cpp

---
+++ 字符串/043. 字符串相乘.cpp
@@ -0,0 +1,24 @@
+43. 字符串相乘
+
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int m = num1.size(), n = num2.size();
+        vector<int> ans(m + n, 0);
+        for (int i = m - 1; i >= 0; --i)
+            for (int j = n - 1; j >= 0; --j) {
+                int mul = (num1[i] - '0') * (num2[j] - '0');
+                int p1 = i + j, p2 = i + j + 1;
+                int sum = mul + ans[p2];
+                ans[p2] = sum % 10;
+                ans[p1] += sum / 10;
+            }
+        int i = 0;
+        while (i < ans.size() && ans[i] == 0) ++i;
+        string str;
+        for (; i < ans.size(); ++i) str.push_back('0' + ans[i]);
+        return str.size() == 0 ? "0" : str;
+    }
+};
