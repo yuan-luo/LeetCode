a95c26a38c84b893861a7515600abd936b93a0ff
Yuan Luo <yuan.luo@hotmail.com>
2021-01-05 22:27:39 +0800

    Update 043. 字符串相乘.cpp

--- 字符串/043. 字符串相乘.cpp
+++ 字符串/043. 字符串相乘.cpp
@@ -6,7 +6,9 @@ class Solution {
 public:
     string multiply(string num1, string num2) {
         int m = num1.size(), n = num2.size();
+        // 结果最多为m + n位数
         vector<int> ans(m + n, 0);
+        // 从个位数开始逐位相乘
         for (int i = m - 1; i >= 0; --i)
             for (int j = n - 1; j >= 0; --j) {
                 int mul = (num1[i] - '0') * (num2[j] - '0');
@@ -15,8 +17,10 @@ public:
                 ans[p2] = sum % 10;
                 ans[p1] += sum / 10;
             }
+        // 结果前缀可能存的0（未使用的位）
         int i = 0;
         while (i < ans.size() && ans[i] == 0) ++i;
+        // 将计算结果转化为字符串
         string str;
         for (; i < ans.size(); ++i) str.push_back('0' + ans[i]);
         return str.size() == 0 ? "0" : str;
