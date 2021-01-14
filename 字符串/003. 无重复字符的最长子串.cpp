a0761dc2cd19c6d6fa4da097dec1a33b78aa4bb9
Yuan Luo <yuan.luo@hotmail.com>
2021-01-14 20:14:13 +0800

    Update 003. 无重复字符的最长子串.cpp

--- 字符串/003. 无重复字符的最长子串.cpp
+++ 字符串/003. 无重复字符的最长子串.cpp
@@ -1,25 +1,7 @@
 3. 无重复字符的最长子串
 
 https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
-
 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-
-示例 1:
-
-输入: "abcabcbb"
-输出: 3 
-解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-示例 2:
-
-输入: "bbbbb"
-输出: 1
-解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-示例 3:
-
-输入: "pwwkew"
-输出: 3
-解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 
 题解：
 
@@ -39,6 +21,24 @@ https://www.cnblogs.com/grandyang/p/4480780.html
 映射对更新为 a->1，那么此时 i-left 还为1，不用更新结果 res，那么最终结果 res 还为1。正确。
 
 代码如下：
+
+我的写法，left为0，如果冲突，left为m[s[i]]+1：
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_map<char, int> m; //key -> index;
+        int ans = 0, left = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            //如果存在m[s[i]]
+            if (m.count(s[i]) && m[s[i]] >= left) {
+                left = m[s[i]] + 1;
+            }
+            m[s[i]] = i;
+            ans = max(ans, i - left + 1);
+        }
+        return ans;
+    }
+};
 
 class Solution {
 public:
