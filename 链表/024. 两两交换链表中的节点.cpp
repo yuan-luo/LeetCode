1b9ef80739347583f75654762fe2891c463fd998
Yuan Luo <yuan.luo@hotmail.com>
2021-01-16 12:57:39 +0800

    Update 024. 两两交换链表中的节点.cpp

--- 链表/024. 两两交换链表中的节点.cpp
+++ 链表/024. 两两交换链表中的节点.cpp
@@ -1,44 +1,12 @@
 24. 两两交换链表中的节点
 
-https://leetcode-cn.com/problems/swap-nodes-in-pairs/
-
-给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
-
-你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
-
-示例 1：
-
-输入：head = [1,2,3,4]
-输出：[2,1,4,3]
-示例 2：
-
-输入：head = []
-输出：[]
-示例 3：
-
-输入：head = [1]
-输出：[1]
-
-提示：
-
-链表中节点的数目在范围 [0, 100] 内
-0 <= Node.val <= 100
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if (!head || !head->next) return head;
-        ListNode *t = head->next;
-        head->next = swapPairs(head->next->next);
-        t->next = head;
-        return t;
+        if (head == nullptr || head->next == nullptr) return head;
+        ListNode *tmp = head->next;
+        head->next = swapPairs(tmp->next);
+        tmp->next = head;
+        return tmp;
     }
 };
