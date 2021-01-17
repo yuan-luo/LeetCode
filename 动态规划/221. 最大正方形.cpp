cf281538a393effcc47db5a287d48a939026ec2b
Yuan Luo <yuan.luo@hotmail.com>
2021-01-17 22:22:40 +0800

    Update 221. 最大正方形.cpp

--- 动态规划/221. 最大正方形.cpp
+++ 动态规划/221. 最大正方形.cpp
@@ -12,6 +12,27 @@ matrix = [["1","0","1","0","0"],
 
 输出：4
 
+写法一：
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) return 0;
+        int m = matrix.size(), n = matrix[0].size(), res = 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0) dp[i][j] = matrix[i][j] - '0';
+                else if (matrix[i][j] == '1') {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                }
+                res = max(res, dp[i][j]);
+            }
+        }
+        return res * res;
+    }
+};
+
+写法二：
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
