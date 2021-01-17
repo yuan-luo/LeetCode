a489e4d70398272ed6341c8b5a941045a5b0eed4
Yuan Luo <yuan.luo@hotmail.com>
2021-01-17 18:54:34 +0800

    Create 10. 正则表达式匹配.cpp

---
+++ 字符串/10. 正则表达式匹配.cpp
@@ -0,0 +1,15 @@
+10. 正则表达式匹配
+
+给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p.size() > 1 && p[1] == '*') {
+            return isMatch(s, p.substr(2)) || (!s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p));
+        } else {
+            return !s.empty() && (s[0] == p[0] || p[0] == '.') && isMatch(s.substr(1), p.substr(1));
+        }
+    }
+};
