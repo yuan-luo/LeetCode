bfd66182a3849b096ecc98a1df65076934976070
Yuan Luo <yuan.luo@hotmail.com>
2021-01-18 22:12:06 +0800

    Update 069. x 的平方根.cpp

--- 二分查找/069. x 的平方根.cpp
+++ 二分查找/069. x 的平方根.cpp
@@ -1,10 +1,29 @@
 69. x 的平方根
 
 实现 int sqrt(int x) 函数。
-
 计算并返回 x 的平方根，其中 x 是非负整数。
-
 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+更好理解：
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        int left = 1, right = x;
+        if (x == 0) return 0;
+        int ans;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid > x / mid) {
+                right = mid - 1;
+            } else {
+                if (mid + 1 > x / (mid + 1)) return mid;
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
 
 class Solution {
 public:
