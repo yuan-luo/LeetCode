cc15e9b6fadaff1ad1e37e0d1877f6f2aa260692
Yuan Luo <yuan.luo@hotmail.com>
2021-01-19 16:34:53 +0800

    Update 048. 旋转图像.cpp

--- 数组/048. 旋转图像.cpp
+++ 数组/048. 旋转图像.cpp
@@ -1,30 +1,34 @@
 48. 旋转图像
 
-https://leetcode-cn.com/problems/rotate-image/
-
 给定一个 n × n 的二维矩阵表示一个图像。
-
 将图像顺时针旋转 90 度。
 
-说明：
-
-你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
-
 示例 1:
-
 给定 matrix = 
 [
   [1,2,3],
   [4,5,6],
   [7,8,9]
 ],
-
-原地旋转输入矩阵，使其变为:
+使其变为:
 [
   [7,4,1],
   [8,5,2],
   [9,6,3]
 ]
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                swap(matrix[i][j], matrix[n - j - 1][n - i - 1]);
+            }
+        }
+        reverse(matrix.begin(), matrix.end());
+    }
+};
 
 class Solution {
 public:
