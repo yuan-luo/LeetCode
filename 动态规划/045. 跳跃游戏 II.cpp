74dab77beb1b0a1172ddcd87e28016730dd34e76
Yuan Luo <yuan.luo@hotmail.com>
2021-01-19 09:29:44 +0800

    Update 045. 跳跃游戏 II.cpp

--- 动态规划/045. 跳跃游戏 II.cpp
+++ 动态规划/045. 跳跃游戏 II.cpp
@@ -1,16 +1,28 @@
+45. 跳跃游戏 II
+
 给定一个非负整数数组，你最初位于数组的第一个位置。
-
 数组中的每个元素代表你在该位置可以跳跃的最大长度。
-
 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
 
-示例:
-
-输入: [2,3,1,1,4]
-输出: 2
-解释: 跳到最后一个位置的最小跳跃数是 2。
-     从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
 说明:
-
 假设你总是可以到达数组的最后一个位置。
 
+reach，当前能达到的最远。
+curEnd，上一轮跳达到的最远。
+如果当前位置到达了 curEnd，即上一步能到达的最远位置，
+说明需要再跳一次。
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jump = 0, reach = 0, curEnd = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            reach = max(reach, i + nums[i]);
+            if (i == curEnd) {
+                curEnd = reach;
+                ++jump;
+            }
+        }
+        return jump;
+    }
+};
