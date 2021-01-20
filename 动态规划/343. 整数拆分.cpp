4d32fb85cec88ba8877e8b5675ce9e94b39f43f8
Yuan Luo <yuan.luo@hotmail.com>
2021-01-20 20:55:07 +0800

    Update 343. 整数拆分.cpp

--- 动态规划/343. 整数拆分.cpp
+++ 动态规划/343. 整数拆分.cpp
@@ -5,13 +5,11 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int> dp(n + 1);
-        for (int i = 2; i <= n; ++i) {
-            int curMax = 0;
-            for (int j = 1; j < i; ++j) {
-                curMax = max(curMax, max(j * (i - j), j * dp[i - j]));
+        vector<int> dp(n + 1, 0);
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]));
             }
-            dp[i] = curMax;
         }
         return dp.back();
     }
