27f560e85b3d8a9e87556b4af4fc366ed339b32b
Yuan Luo <yuan.luo@hotmail.com>
2021-01-22 21:16:39 +0800

    Update 416. 分割等和子集.cpp

--- 动态规划/416. 分割等和子集.cpp
+++ 动态规划/416. 分割等和子集.cpp
@@ -17,6 +17,15 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-
+        int sum = accumulate(nums.begin(), nums.end(), 0), target = sum >> 1;
+        if (sum & 1) return false;
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = target; i >= num; --i) {
+                dp[i] = dp[i] || dp[i - num];
+            }
+        }
+        return dp[target];
     }
 };
