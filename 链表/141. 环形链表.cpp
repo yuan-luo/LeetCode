f0a6da8ecf0b6791120f9da808b2c5f0c833bbba
Yuan Luo <yuan.luo@hotmail.com>
2021-01-22 21:33:02 +0800

    Update and rename *141. 环形链表.cpp to 141. 环形链表.cpp

---
+++ 链表/141. 环形链表.cpp
@@ -0,0 +1,14 @@
+141. 环形链表
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+};
