ec1780298ebae4117348cff73bdf61e3b4d64230
Yuan Luo <yuan.luo@hotmail.com>
2021-01-22 23:31:45 +0800

    Update 209. 长度最小的子数组.cpp

--- 数组/209. 长度最小的子数组.cpp
+++ 数组/209. 长度最小的子数组.cpp
@@ -7,8 +7,7 @@
 class Solution {
 public:
     int minSubArrayLen(int s, vector<int>& nums) {
-        int left = 0;
-        int ans = INT_MAX, sum = 0;
+        int sum = 0, left = 0, ans = INT_MAX;
         for (int i = 0; i < nums.size(); ++i) {
             sum += nums[i];
             while (sum >= s) {
