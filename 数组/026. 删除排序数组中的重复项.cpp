49c1790692cd1e5f8a63d2f9993394bb1682d13b
Yuan Luo <yuan.luo@hotmail.com>
2021-01-23 20:18:41 +0800

    Update 026. 删除排序数组中的重复项.cpp

--- 数组/026. 删除排序数组中的重复项.cpp
+++ 数组/026. 删除排序数组中的重复项.cpp
@@ -3,30 +3,16 @@
 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 
-示例 1:
-给定数组 nums = [1,1,2], 
-函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
-你不需要考虑数组中超出新长度后面的元素。
-
-示例 2:
-给定 nums = [0,0,1,1,1,2,2,3,3,4],
-函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
-你不需要考虑数组中超出新长度后面的元素。
- 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int size = nums.size();
-        if(size == 0) return 0;
-        int n = 0;
-        for(int i = 0; i < size; i++){
-            if(nums[i] == nums[n]){
-                continue;
-            } else {
-                n++;
-                nums[n] = nums[i];
+        if (nums.empty()) return 0;
+        int slow = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[slow]) {
+                nums[++slow] = nums[i];
             }
         }
-        return n + 1;
+        return slow + 1;
     }
 };
