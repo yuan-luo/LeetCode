c40f5d792e99c1c23139c163afb14ccfdd91275c
Yuan Luo <yuan.luo@hotmail.com>
2021-01-23 20:40:29 +0800

    Update 203. 移除链表元素.cpp

--- 链表/203. 移除链表元素.cpp
+++ 链表/203. 移除链表元素.cpp
@@ -2,11 +2,26 @@
 
 删除链表中等于给定值 val 的所有节点。
 
+递归：
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
         if (head == nullptr) return nullptr;
         head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
+    }
+};
+
+非递归：
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if (head == nullptr) return nullptr;
+        ListNode *cur = head;
+        while (cur->next) {
+            if (cur->next->val == val) cur->next = cur->next->next;
+            else cur = cur->next;
+        }
         return head->val == val ? head->next : head;
     }
 };
