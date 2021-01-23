d9f6ac9fb21d507ab5fd727c4791096a560177ec
Yuan Luo <yuan.luo@hotmail.com>
2021-01-23 21:04:00 +0800

    Update 151. 翻转字符串里的单词.cpp

--- 字符串/151. 翻转字符串里的单词.cpp
+++ 字符串/151. 翻转字符串里的单词.cpp
@@ -81,4 +81,24 @@ public:
     }
 };
 
-
+class Solution {
+public:
+    string reverseWords(string s) {
+        int storeIndex = 0, n = s.size();
+        reverse(s.begin(), s.end());
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] != ' ') {
+                if (storeIndex != 0) s[storeIndex++] = ' ';
+                int j = i;
+                while (j < s.size() && s[j] != ' ') {
+                    s[storeIndex++] = s[j++];
+                }
+                reverse(s.begin() + storeIndex - (j - i), s.begin() + storeIndex);
+                i = j;
+            }
+        }
+        s.resize(storeIndex);
+        return s;
+    }
+};
+                             
