c75ae2731f2595126182f27f0dff63b43101b40c
Yuan Luo <yuan.luo@hotmail.com>
2021-01-26 20:08:34 +0800

    Update 067. 二进制求和.cpp

--- 字符串/067. 二进制求和.cpp
+++ 字符串/067. 二进制求和.cpp
@@ -3,41 +3,19 @@
 给你两个二进制字符串，返回它们的和（用二进制表示）。
 输入为 非空 字符串且只包含数字 1 和 0。
 
-示例 1:
-
-输入: a = "11", b = "1"
-输出: "100"
-示例 2:
-
-输入: a = "1010", b = "1011"
-输出: "10101"
- 
-提示：
-
-每个字符串仅由字符 '0' 或 '1' 组成。
-1 <= a.length, b.length <= 10^4
-字符串如果不是 "0" ，就都不含前导零。
-
-题解：
-
-先将两数倒序，然后设计一个carry进位。最后把结果再倒序。
-
 class Solution {
 public:
     string addBinary(string a, string b) {
+        int m = a.size() - 1, n = b.size() - 1, carry = 0;
         string ans;
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        int n = max(a.size(), b.size()), carry = 0;
-        for (int i = 0; i < n; ++i) {
-            carry += i < a.size() ? (a.at(i) == '1') : 0;
-            carry += i < b.size() ? (b.at(i) == '1') : 0;
-            if (carry % 2 == 1) ans.push_back('1');
-            else ans.push_back('0');
-            carry /= 2;
+        while (m >= 0 || n >= 0) {
+            int v1 = m >= 0 ? a[m--] - '0' : 0;
+            int v2 = n >= 0 ? b[n--] - '0' : 0;
+            int sum = v1 + v2 + carry;
+            ans = to_string(sum % 2) + ans;
+            carry = sum / 2;
         }
-        if (carry) ans.push_back('1');
-        reverse(ans.begin(), ans.end());
+        if (carry > 0) ans = '1' + ans;
         return ans;
     }
 };
