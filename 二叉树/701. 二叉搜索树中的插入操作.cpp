26bd16319a98bfc2abb51bde753ec70a5cf2def7
Yuan Luo <yuan.luo@hotmail.com>
2021-02-25 19:24:32 +0800

    Update 701. 二叉搜索树中的插入操作.cpp

--- 二叉树/701. 二叉搜索树中的插入操作.cpp
+++ 二叉树/701. 二叉搜索树中的插入操作.cpp
@@ -1,8 +1,11 @@
 701. 二叉搜索树中的插入操作
 
-给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 返回插入后二叉搜索树的根节点。 输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
+给定二叉搜索树（BST）的根节点和要插入树中的值，将值插入二叉搜索树。 
+返回插入后二叉搜索树的根节点。 
+输入数据 保证 ，新值和原始二叉搜索树中的任意节点值都不同。
 
-注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 你可以返回 任意有效的结果 。
+注意，可能存在多种有效的插入方式，只要树在插入后仍保持为二叉搜索树即可。 
+你可以返回 任意有效的结果 。
 
 class Solution {
 public:
@@ -27,10 +30,16 @@ public:
         TreeNode *cur = root;
         while (true) {
             if (val < cur->val) {
-                if (cur->left == nullptr) { cur->left = new TreeNode(val); break; }
+                if (cur->left == nullptr) { 
+                    cur->left = new TreeNode(val); 
+                    break; 
+                }
                 cur = cur->left;
             } else {
-                if (cur->right == nullptr) { cur->right = new TreeNode(val); break; }
+                if (cur->right == nullptr) {
+                    cur->right = new TreeNode(val); 
+                    break; 
+                }
                 cur = cur->right;
             }
         }
