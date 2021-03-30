82e39e664173296d19c2c2a4266d89848707e0dc
Yuan Luo <yuan.luo@hotmail.com>
2021-03-30 22:41:19 +0800

    Update 101. 对称二叉树.cpp

--- 二叉树/101. 对称二叉树.cpp
+++ 二叉树/101. 对称二叉树.cpp
@@ -25,3 +25,21 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (!root) return true;
+        return isSymmetric(root->left, root->right);
+    }
+    
+    bool isSymmetric(TreeNode *left, TreeNode *right) {
+        if (!left && !right) return true;
+        if (left && !right || !left && right || 
+            left->val != right->val) return false;
+        return isSymmetric(left->left, right->right) && 
+            isSymmetric(left->right, right->left);
+    }
+    
+};
