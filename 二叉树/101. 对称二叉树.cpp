68ef65fbfe8358d0c8b7354da9a1926f75200265
Yuan Luo <yuan.luo@hotmail.com>
2021-03-30 22:39:08 +0800

    Create 101. 对称二叉树.cpp

---
+++ 二叉树/101. 对称二叉树.cpp
@@ -0,0 +1,27 @@
+101. 对称二叉树
+
+给定一个二叉树，检查它是否是镜像对称的。
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if (root == nullptr) return true;
+        queue<TreeNode*> q1, q2;
+        q1.push(root->left); q2.push(root->right);
+        while (!q1.empty() && !q2.empty()) {
+            TreeNode *node1 = q1.front(); q1.pop();
+            TreeNode *node2 = q2.front(); q2.pop();
+            if ((node1 && !node2) || (!node1 && node2)) 
+                return false;
+            if (node1 != nullptr) {
+                if (node1->val != node2->val) return false;
+                q1.push(node1->left);
+                q2.push(node2->right);
+                q1.push(node1->right);
+                q2.push(node2->left);
+            }
+
+        }
+        return true;
+    }
+};
