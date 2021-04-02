eb17971ab49c37bae725d7a38bb29ec2a919a29f
Yuan Luo <yuan.luo@hotmail.com>
2021-04-02 14:00:19 +0800

    Update 322. 零钱兑换.cpp

--- 动态规划/322. 零钱兑换.cpp
+++ 动态规划/322. 零钱兑换.cpp
@@ -1,6 +1,8 @@
 322. 零钱兑换
 
-给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+给定不同面额的硬币 coins 和一个总金额 amount。
+编写一个函数来计算可以凑成总金额所需的最少的硬币个数。
+如果没有任何一种硬币组合能组成总金额，返回 -1。
 
 你可以认为每种硬币的数量是无限的。
 
@@ -17,7 +19,8 @@ public:
         dp[0] = 0;
         for (int i = 0; i <= amount; ++i) {
             for (int j = 0; j < coins.size(); ++j) {
-                if (coins[j] <= i) dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
+                if (coins[j] <= i) 
+                    dp[i] = min(dp[i - coins[j]] + 1, dp[i]);
             }
         }
         return dp[amount] == amount + 1 ? -1 : dp[amount];
