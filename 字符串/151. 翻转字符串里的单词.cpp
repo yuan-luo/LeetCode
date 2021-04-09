45439932e4cab596f46bbc38eb74cd51e9e2760a
Yuan Luo <yuan.luo@hotmail.com>
2021-04-09 16:01:22 +0800

    Update 151. 翻转字符串里的单词.cpp

--- 字符串/151. 翻转字符串里的单词.cpp
+++ 字符串/151. 翻转字符串里的单词.cpp
@@ -1,29 +1,5 @@
 151. 翻转字符串里的单词
 
-https://leetcode-cn.com/problems/reverse-words-in-a-string/
-
-给定一个字符串，逐个翻转字符串中的每个单词。
-
-说明：
-
-无空格字符构成一个 单词 。
-输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
-如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
- 
-示例 1：
-输入："the sky is blue"
-输出："blue is sky the"
-    
-示例 2：
-输入："  hello world!  "
-输出："world! hello"
-解释：输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
-
-示例 3：
-输入："a good   example"
-输出："example good a"
-解释：如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
-
 示例 4：
 输入：s = "  Bob    Loves  Alice   "
 输出："Alice Loves Bob"
