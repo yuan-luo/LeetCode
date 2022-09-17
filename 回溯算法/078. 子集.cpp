430a911a7a9067716c009b5e6e17cd96b6881325
Yuan Luo <yuan.luo@hotmail.com>
2022-09-17 17:50:31 +0800

    Update 078. 子集.cpp

--- 回溯算法/078. 子集.cpp
+++ 回溯算法/078. 子集.cpp
@@ -40,3 +40,29 @@ public:
         }
     }
 };
+
+下面这个方案感觉更清晰，更直观。cur代表当前要加入的待选数组。
+当n==cur.size()，说明当前已经找到一个答案，加入到ans中。
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> cur;
+        for(int i = 0; i <= nums.size(); ++i) {
+            dfs(nums, i, 0, cur, ans);    
+        }
+        return ans;
+    }
+    void dfs(const vector<int>& nums, int n, int s, vector<int> cur, vector<vector<int>>& ans) {
+        if (n == cur.size()) {
+            ans.push_back(cur);
+            return;
+        }
+        for (int i = s; i < nums.size(); ++i) {
+            cur.push_back(nums[i]);
+            dfs(nums, n, i + 1, cur, ans);
+            cur.pop_back();
+        }
+    }
+
+};
