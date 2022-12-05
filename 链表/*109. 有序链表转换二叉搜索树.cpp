6b9901f388febf09cc7d2fe39067ead8bba59820
Yuan Luo <yuan.luo@hotmail.com>
2022-12-05 10:27:21 +0800

    Update *109. 有序链表转换二叉搜索树.cpp

--- 链表/*109. 有序链表转换二叉搜索树.cpp
+++ 链表/*109. 有序链表转换二叉搜索树.cpp
@@ -1,46 +1,32 @@
 109. 有序链表转换二叉搜索树
-
 给定一个单链表，其中的元素按升序排序，将其转换为高度平衡的二叉搜索树。
-
 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
 
 示例:
-
 给定的有序链表： [-10, -3, 0, 5, 9],
-
 一个可能的答案是：[0, -3, 9, -10, null, 5], 它可以表示下面这个高度平衡二叉搜索树：
-
       0
      / \
    -3   9
    /   /
  -10  5
 
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
-        
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return new TreeNode(head->val);
+        ListNode *slow = head, *fast = head, *pre = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            pre = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        fast = slow->next;
+        pre->next = nullptr;
+        TreeNode* root = new TreeNode(slow->val);
+        if (head != slow) root->left = sortedListToBST(head);
+        root->right = sortedListToBST(fast);
+        return root;
     }
 };
