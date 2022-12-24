77201a6e8a10d2a6bc1033e685d0fb09eb3c1f7b
Yuan Luo <yuan.luo@hotmail.com>
2022-12-24 11:24:52 +0800

    Update 091. 解码方法.cpp

--- 动态规划/091. 解码方法.cpp
+++ 动态规划/091. 解码方法.cpp
@@ -17,15 +17,15 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        if (s.empty() || s[0] == '0') return 0;
-        vector<int> dp(s.size() + 1, 0);
-        dp[0] = 1;
-        for (int i = 1; i < dp.size(); ++i) {
-            dp[i] = s[i - 1] == '0' ? 0 : dp[i - 1];
-            if (i > 1 && (s[i - 2] == '1' || (s[i - 2] == '2' && s[i - 1] <= '6'))) {
-                dp[i] += dp[i - 2];
+        vector<int> f(s.size() + 1, 0);
+        f[0] = 1;
+        for (int i = 1; i <= s.size(); ++i) {
+            if (s[i - 1] != '0') f[i] = f[i - 1];
+            if (i >= 2) {
+                int t = (s[i - 2] - '0') * 10 + s[i - 1] - '0';
+                if (t >= 10 && t <= 26) f[i] += f[i - 2];
             }
         }
-        return dp.back();
+        return f.back();
     }
 };
