d60bf6b44dffa27c593d91ee5812a9144543c9f8
Yuan Luo <yuan.luo@hotmail.com>
2022-12-24 09:41:37 +0800

    Update 131. 分割回文串.cpp

--- 字符串/131. 分割回文串.cpp
+++ 字符串/131. 分割回文串.cpp
@@ -1,11 +1,9 @@
 131. 分割回文串
 
 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
-
 返回 s 所有可能的分割方案。
 
 示例:
-
 输入: "aab"
 输出:
 [
@@ -13,3 +11,30 @@
   ["a","a","b"]
 ]
 
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> out;
+        helper(s, 0, out, ans);
+        return ans;
+    }
+    void helper(string s, int start, vector<string>& out,
+                vector<vector<string>>& ans) {
+        if (start == s.size()) { ans.push_back(out); return; }
+        for (int i = start; i < s.size(); ++i) {
+            if (!isPalindrome(s, start, i)) continue;
+            out.push_back(s.substr(start, i - start + 1));
+            helper(s, i + 1, out, ans);
+            out.pop_back();
+        }
+    }
+    bool isPalindrome(string s, int start, int end) {
+        while (start < end) {
+            if (s[start] != s[end]) return false;
+            ++start;
+            --end;
+        }
+        return true;
+    }
+};
