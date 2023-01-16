f970ff12e96d4fe24af320e62344d49bc67b8808
Yuan Luo <yuan.luo@hotmail.com>
2023-01-16 21:54:23 +0800

    Update 347. 前 K 个高频元素.cpp

--- 堆/347. 前 K 个高频元素.cpp
+++ 堆/347. 前 K 个高频元素.cpp
@@ -1,38 +1,23 @@
 347. 前 K 个高频元素
-
 https://leetcode-cn.com/problems/top-k-frequent-elements/
-
 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
 
 示例 1:
-
 输入: nums = [1,1,1,2,2,3], k = 2
 输出: [1,2]
-示例 2:
 
+示例 2:
 输入: nums = [1], k = 1
 输出: [1]
- 
-提示：
-
-你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
-你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
-题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
-你可以按任意顺序返回答案。
-
 
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> m; // val:freq;
-        vector<int> res;
+        unordered_map<int, int> m; // val->freq;
         priority_queue<pair<int, int>> q;
-        for (auto i : nums) {
-            m[i]++;
-        }
-        for (auto it : m) {
-            q.push(pair<int, int>(it.second, it.first));
-        }
+        vector<int> res;
+        for (auto a : nums) ++m[a];
+        for (auto it : m) q.push({it.second, it.first});
         for (int i = 0; i < k; ++i) {
             res.push_back(q.top().second);
             q.pop();
