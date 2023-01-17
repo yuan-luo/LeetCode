59d3b97b99e851ed22edea6bb50c5e5df68a3382
Yuan Luo <yuan.luo@hotmail.com>
2023-01-17 10:29:08 +0800

    Update 070. 爬楼梯.cpp

--- 动态规划/070. 爬楼梯.cpp
+++ 动态规划/070. 爬楼梯.cpp
@@ -7,6 +7,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        vector<int> dp(n + 1, 0);
+        if (n <= 2) return n;
+        dp[0] = 1; dp[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp.back();
+    }
+};
+
+class Solution {
+public:
+    int climbStairs(int n) {
         int p = 0, q = 0, r = 1;
         for (int i = 1; i <= n; ++i) {
             p = q; 
